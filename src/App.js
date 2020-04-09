da60b67241fb1580b983d50ea9a60b926028b22f
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-09 17:55:12 -0300

    Add the logic to get the data from the backend and list in the frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(reponse => {
+      setRepositories(reponse.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    alert(`removed id ${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => <li key={repository.id}>{repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
